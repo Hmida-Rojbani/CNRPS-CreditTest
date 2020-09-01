43c8a8ad6b6ea2800952f95a70f729bc0da1f5e7
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-09-01 12:18:00 +0100

    Env

---
+++ src/main/java/tn/cnrps/ws/soap/CreditTestApplication.java
@@ -0,0 +1,13 @@
+package tn.cnrps.ws.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditTestApplication.class, args);
+	}
+
+}
